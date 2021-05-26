9e53bf4510260226079db84027010706ac9a2c28
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-25 19:01:31 -0600

    Create Add a Pet to an Application #22

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -12,4 +12,8 @@ class Pet < ApplicationRecord
   def self.adoptable
     where(adoptable: true)
   end
+
+  def self.pet_search(name_part)
+    where("name LIKE ?", "%#{name_part}%")
+  end
 end
--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 
 RSpec.describe ApplicationPet, type: :model do
   describe "relationships" do
+    it { should validate_presence_of(:application) }
+    it { should validate_presence_of(:pet) }
     it {should belong_to :application}
     it {should belong_to :pet}
   end
